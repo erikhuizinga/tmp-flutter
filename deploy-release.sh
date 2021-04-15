cf4457c77825eff21c9b52628312878d7f297132
Erik Huizinga <erikhuizinga@users.noreply.github.com>
2021-04-15 15:47:08 +0200

    Update release script

--- deploy-release.sh
+++ deploy-release.sh
@@ -1,13 +1,12 @@
-git reset
-git checkout .
 flutter build web --release
 git add -f build/web
-git stash -u
+git stash
 git checkout gh-pages
 git clean -xdf
-git stash pop
-mv build/web/* .
-git add -A
+git rm -rf .
 git commit -m "[Auto] release @ $(date)"
+git stash pop
+git mv build/web/* .
+git commit --amend --no-edit
 git push
 git checkout master
